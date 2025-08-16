bd1bb0c89384ac65b8e4d44fc7ae0479c0a34b73
PranayDara09 <pranaydara09@gmail.com>
2025-08-17 04:08:10 +0530

    typo fixes

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Login.css';
+import './login.css';
 
 function Login() {
   const vendorDemo = { email: 'vendor@demo.com', password: '12345678' };
